2b72cbe176a64b96d1cb298ad4c4c4e5562416bd
David Revelo <drevelo@stackbuilders.com>
2016-05-26 11:47:38 -0500

    Adds the products create, update and destroy action nested on the user resources

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,12 @@ MarketPlaceApi::Application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: {subdomain: 'api'}, path: '/' do
 
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, :only => [:show, :create, :update, :destroy]
-      resources :sessions, :only => [:create, :destroy]
-      resources :products, :only => [:show, :index]
+      resources :users, only: [:show, :create, :update, :destroy] do
+        # this is the line
+        resources :products, only: [:create, :update, :destroy]
+      end
+      resources :sessions, only: [:create, :destroy]
+      resources :products, only: [:show, :index]
     end
 
   end
